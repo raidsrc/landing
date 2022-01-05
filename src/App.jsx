42d272e06add61500fb0a11a26cde9c97cd9844e
Ray Louis <x1337c@gmail.com>
2022-01-05 00:01:01 -0800

    you know what? never mind. that back button  is too fucking baller to leave out

--- src/App.jsx
+++ src/App.jsx
@@ -88,35 +88,35 @@ function PrimaryLandingPage(props) {
 
 function SupPage(props) {
   const setShowSupPage = props.setShowSupPage
-  // const [backButtonTranslucency, setBackButtonTranslucency] = useState("opacity-100")
-  // const [backArrowTransparency, setBackArrowTransparency] = useState("opacity-100")
-  // const [triggerBackButtonAnimation, setTriggerBackButtonAnimation] = useState(false)
+  const [backButtonTranslucency, setBackButtonTranslucency] = useState("opacity-100")
+  const [backArrowTransparency, setBackArrowTransparency] = useState("opacity-100")
+  const [triggerBackButtonAnimation, setTriggerBackButtonAnimation] = useState(false)
   const imgRef = useRef(null) // first time using a ref ever. a little confusing.
 
-  // function trackScrolling() {
-  //   if (window.scrollY > imgRef.current.height - 50 && window.innerWidth < 768) {
-  //     setTimeout(() => { setBackButtonTranslucency("opacity-20") }, 400)
-  //     setTimeout(() => { setBackArrowTransparency("opacity-10") }, 400)
-  //     setTriggerBackButtonAnimation(true)
-  //   } else {
-  //     setTimeout(() => { setBackButtonTranslucency("opacity-100") }, 400)
-  //     setTimeout(() => { setBackArrowTransparency("opacity-100") }, 400)
-  //     setTriggerBackButtonAnimation(false)
-  //   }
-  // }
+  function trackScrolling() {
+    if (window.scrollY > imgRef.current.height - 50 && window.innerWidth < 768) {
+      setTimeout(() => { setBackButtonTranslucency("opacity-20") }, 400)
+      setTimeout(() => { setBackArrowTransparency("opacity-10") }, 400)
+      setTriggerBackButtonAnimation(true)
+    } else {
+      setTimeout(() => { setBackButtonTranslucency("opacity-100") }, 400)
+      setTimeout(() => { setBackArrowTransparency("opacity-100") }, 400)
+      setTriggerBackButtonAnimation(false)
+    }
+  }
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", trackScrolling)
-  //   window.addEventListener("resize", trackScrolling)
-  //   return () => {
-  //     window.removeEventListener("scroll", trackScrolling)
-  //     window.removeEventListener("resize", trackScrolling)
-  //   }
-  // }, [window.innerWidth])
+  useEffect(() => {
+    window.addEventListener("scroll", trackScrolling)
+    window.addEventListener("resize", trackScrolling)
+    return () => {
+      window.removeEventListener("scroll", trackScrolling)
+      window.removeEventListener("resize", trackScrolling)
+    }
+  }, [window.innerWidth])
 
   return (
     <div>
-      {/* <CSSTransition timeout={400} in={triggerBackButtonAnimation} classNames="translucent-button">
+      <CSSTransition timeout={400} in={triggerBackButtonAnimation} classNames="translucent-button">
         <div className={"fixed left-7 top-6 md:left-auto md:top-auto " + backButtonTranslucency} >
           <button className="outline outline-1 hover:outline-white hover:ease-in duration-150 active:opacity-80 md:hover:opacity-100" 
           onClick={() => setShowSupPage(false)}>
@@ -127,7 +127,7 @@ function SupPage(props) {
             </CSSTransition>
           </button>
         </div>
-      </CSSTransition> */}
+      </CSSTransition>
       <div className="flex flex-row justify-center pt-7 sm:pt-4 md:pt-0">
         <img src={hairBlown} ref={imgRef} className="rounded-full w-7/12 
                 tiny-screen:w-6/12 
@@ -166,11 +166,11 @@ function SupPage(props) {
           <p>
 
           </p>
-          <div className="flex flex-row justify-center py-10">
+          {/* <div className="flex flex-row justify-center py-10">
             <button onClick={() => { setShowSupPage(false); scroll(0, 0) }} className="inline-red-bg-link text-white border py-2 px-2 w-28 md:w-40 md:px-0 duration-150 hover:ease-in hover:border-gray-800 hover:opacity-100 active:opacity-80">
               See you around.
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
 
